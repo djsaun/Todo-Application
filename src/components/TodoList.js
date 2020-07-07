58748b2db33e400fde8ac93c661bd62e4482e1d9
David Saunders <djsaun+github@gmail.com>
2020-07-07 13:00:01 -0400

    Exclude completed todos from category list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,8 +7,8 @@ const TodoList = props => {
   return (
     <>
       {categories && categories.map(category => {
-        // Filter todos by category and pass them as props to their assigned category
-        const filteredTodos = todos.filter(todo => todo.category === category.id)
+        // Filter todos by category and completed status and pass them as props to their assigned category
+        const filteredTodos = todos.filter(todo => todo.category === category.id && !todo.isCompleted )
         return <Category key={category.id} id={category.id} title={category.title} todos={filteredTodos} categories={categories} />
       })}
     </>
